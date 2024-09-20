8a90b92e5cb63837b1da2218e6ddb6b463b5de32
fmjal <felixaltenbach0@gmail.com>
2024-09-20 17:45:23 +0200

    .

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for i in `ls bin`; do
     sudo setcap cap_net_bind_service=+ep ./bin/$i
-    sudo strip ./bin/$i
-    sudo upx -f ./bin/$i || true
-done 2> /dev/null > /dev/null
+    strip ./bin/$i
+    upx -f ./bin/$i || true
+done

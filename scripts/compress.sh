0d5948ee86e5364e53b8ee4c8afae25036c59991
user <user@host>
2024-09-24 00:06:06 +0000

    Fix setcap

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -3,4 +3,5 @@ for i in `ls bin`; do
     sudo setcap cap_net_bind_service=+ep ./bin/$i
     strip ./bin/$i
     upx -f ./bin/$i || true
+    sudo setcap cap_net_bind_service=+ep ./bin/$i || true
 done
